dc27654883c59071ccc35e9c47ae507b7ed6edb2
Anindya Dey <30517208+andys-github@users.noreply.github.com>
2022-02-24 23:26:55 +0530

    Add support for GraphCMS (#7)
    * upgrade pkg
    
    * upgrade pkg and add graphql
    
    * redesign blog
    
    * add header and components
    
    * add card component for posts

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,39 @@
-import Link from 'next/link';
+import Head from "next/head";
 
-import { BlogPosts } from '../data/blog-posts';
+import { PostCard, PostWidget, Categories } from "../components";
+import { getPosts } from "../services";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-      <div className="mt-3 grid sm:grid-cols-2 grid-cols-1 gap-4">
-        {
-          BlogPosts.map(blog => (
-            <BlogItem key={blog.slug} {...blog} />
-          ))
-        }
-    </div>
-  )
+    <>
+      <div className="container mx-auto px-10 mb-8">
+        <Head>
+          <title>Anindya | Blog</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+            {posts.map((post, index) => (
+              <PostCard key={index} post={post.node} />
+            ))}
+          </div>
+          <div className="col-span-1 lg:col-span-4">
+            <div className="relative lg:sticky top-8">
+              <PostWidget />
+              <Categories />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-function BlogItem({ title, slug, createdOn, content }) {
-  return (
-    <div className="p-6 border border-gray-300 rounded-lg">
-      <div className="sm:text-2xl text-lg font-bold text-blue-500"><Link href={`blogs/${slug}`}><a>{title}</a></Link></div>
-      <div className="text-sm font-light text-gray-500">Posted on: {createdOn}</div>
-      <div className="smtext-lg text-sm text-gray-700">{content}</div>
-    </div>
-  );
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
