3197d298c2b22ab57af41dd31566190880f3719a
Anindya Dey <anindya.dey@vistaprint.com>
2022-02-25 11:12:48 +0530

    update Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,19 +14,19 @@ const categories = [
 
 const Header = () => {
   return (
-    <div className="container mx-auto mb-8 bg-blue-400 px-10">
-      <div className="inline-block w-full border-b border-blue-400 py-8">
+    <div className="container mx-auto mb-8 px-10">
+      <div className="inline-block w-full border-b border-gray-400 py-6">
         <div className="block md:float-left">
           <Link href="/">
-            <span className="cursor-pointer text-4xl font-bold text-white drop-shadow-md">
-              BLOG
+            <span className="cursor-pointer text-4xl font-light text-gray-800 drop-shadow-lg">
+              Scratchpad
             </span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`category/${category.slug}`}>
-              <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
+              <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-gray-500 md:float-right">
                 {category.name}
               </span>
             </Link>
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -4,7 +4,7 @@ import moment from "moment";
 
 const PostCard = ({ post }) => {
   return (
-    <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
+    <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8 ring-1 ring-gray-200">
       <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
         <img
           src={post.featuredImage.url}
