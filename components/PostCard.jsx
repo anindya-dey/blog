f48a59cd90a21cb51c5f5dc8b1bf25cc26e5e220
Anindya Dey <30517208+andys-github@users.noreply.github.com>
2022-03-06 14:54:31 +0530

    Update packages (#8)
    * upgrade pkg
    
    * upgrade pkg and add graphql
    
    * redesign blog
    
    * add header and components
    
    * add card component for posts
    
    * add prettier
    
    * update Header
    
    * upgrade pkg
    
    * upgrade pkgs
    
    Co-authored-by: Anindya Dey <anindya.dey@vistaprint.com>

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import Link from "next/link";
-import moment from 'moment';
+import moment from "moment";
 
 const PostCard = ({ post }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
-      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+    <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8 ring-1 ring-gray-200">
+      <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-cover absolute h-80 w-full shadow-lg rounded-t-lg lg:rounded-lg"
+          className="absolute h-80 w-full rounded-t-lg object-cover shadow-lg lg:rounded-lg"
         />
       </div>
-      <h1 className="text-3xl font-semibold text-center hover:text-blue-700 transition duration-500 mb-8">
+      <h1 className="mb-8 text-center text-3xl font-semibold transition duration-500 hover:text-blue-700">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div>
-        <div className="flex items-center justify-center mb-4">
-          <img 
+        <div className="mb-4 flex items-center justify-center">
+          <img
             src={post.author.photo.url}
             alt={post.author.name}
             height="50px"
@@ -27,9 +27,7 @@ const PostCard = ({ post }) => {
           <p>{post.author.name}</p>
         </div>
         <div>
-          <span>
-            {moment(post.createdAt).format("MMM DD, YYYY")}
-          </span>
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
         </div>
       </div>
     </div>
