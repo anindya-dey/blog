4c4455a7485b1c404d0dca432b581df85449c7ef
Anindya Dey <andy.ani1992@gmail.com>
2022-07-18 18:08:37 +0530

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,10 @@ import Image from "next/image";
 import { gql } from "graphql-request";
 
 import hygraph from "../util/hygraph";
+import { Post } from "../models";
+import { PostCard } from "../components";
 
-export default function Home({
-  posts,
-}: {
-  posts: {
-    publishedAt: string;
-    title: string;
-    excerpt: string;
-    categories: { slug: string }[];
-  }[];
-}) {
+export default function Home({ posts }: { posts: Post[] }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -25,92 +18,9 @@ export default function Home({
         <h1 className="text-6xl font-bold">Namaste World! 🙏</h1>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Docker 🐋 Network </h3>
-            <p className="mt-4 text-xl">
-              Lets talk about various ways to configure container communication
-              to the outside world 🌏
-            </p>
-            <div className="mt-6">
-              <span className="mr-4 bg-blue-100 px-4 py-2 text-sm text-gray-600 rounded-md">
-                docker
-              </span>
-            </div>
-          </a>
-
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">{posts[0].title}</h3>
-            <h5 className="mt-2 text-sm text-gray-500">
-              {new Date(posts[0].publishedAt).toLocaleDateString("en-gb", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-                timeZone: "utc",
-              })}
-            </h5>
-            <p className="mt-4 text-xl">{posts[0].excerpt}</p>
-            <div className="mt-6">
-              <span className="mr-4 bg-blue-100 px-4 py-2 text-sm text-gray-600 rounded-md">
-                {posts[0].categories[0].slug}
-              </span>
-            </div>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Python 🐍 List &rarr;</h3>
-            <p className="mt-4 text-xl">
-              How does python list work under the hood?
-            </p>
-            <div className="mt-6">
-              <span className="mr-4 bg-blue-100 px-4 py-2 text-sm text-gray-600 rounded-md">
-                python
-              </span>
-            </div>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">
-              Pass by Reference in C++ &rarr;
-            </h3>
-            <p className="mt-4 text-xl">
-              Lets learn how to pass parameters by reference in C++.
-            </p>
-            <div className="mt-6">
-              <span className="mr-4 bg-blue-100 px-4 py-2 text-sm text-gray-600 rounded-md">
-                c++
-              </span>
-            </div>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">YAML syntax&rarr;</h3>
-            <p className="mt-4 text-xl">
-              Lets see how YAML differs from JSON syntax!
-            </p>
-            <div className="mt-6">
-              <span className="mr-4 bg-blue-100 px-4 py-2 text-sm text-gray-600 rounded-md">
-                yaml
-              </span>
-              <span className="mr-4 bg-blue-100 px-4 py-2 text-sm text-gray-600 rounded-md">
-                json
-              </span>
-            </div>
-          </a>
+          {posts.map((post: Post, index: number) => (
+            <PostCard key={index} post={post} />
+          ))}
         </div>
       </main>
 
