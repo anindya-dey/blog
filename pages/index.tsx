6dfa4157ed525018f1f906acf789bf84e9c157b9
Anindya Dey <andy.ani1992@gmail.com>
2022-07-18 20:45:33 +0530

    add Layout to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,38 @@
 import Head from "next/head";
-import Image from "next/image";
 import { gql } from "graphql-request";
 
 import hygraph from "../util/hygraph";
 import { Post } from "../models";
-import { PostCard } from "../components";
+import { PostCard , Footer } from "../components";
 
 export default function Home({ posts }: { posts: Post[] }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <>
       <Head>
         <title>Blog | Anindya Dey</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="space-y-4">
+      <main className="mt-6 space-y-6">
+        {posts.map((post: Post, index: number) => (
+          <PostCard key={index} post={post} />
+        ))}
+
+        {posts.map((post: Post, index: number) => (
+          <PostCard key={index} post={post} />
+        ))}
+
+        {posts.map((post: Post, index: number) => (
+          <PostCard key={index} post={post} />
+        ))}
+
         {posts.map((post: Post, index: number) => (
           <PostCard key={index} post={post} />
         ))}
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+      <Footer />
+    </>
   );
 }
 
@@ -43,6 +44,9 @@ const QUERY = gql`
       excerpt
       author {
         name
+        photo {
+          url
+        }
       }
       publishedAt
       categories {
@@ -61,4 +65,3 @@ export async function getServerSideProps() {
     },
   };
 }
-
